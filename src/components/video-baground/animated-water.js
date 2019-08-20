178a745ebaf262af263e07815d9f896eb3a2e9db
topmonitoring <dodoboy9620@gmail.com>
2019-08-20 16:21:05 +0300

    minor edit

--- src/components/video-baground/animated-water.js
+++ src/components/video-baground/animated-water.js
@@ -1,13 +1,12 @@
 import React from "react"
 import { useTrail, animated } from "react-spring"
-import { render } from "react-dom"
 import "./animated-water.styles.scss"
 
 const fast = { mass: 1, tension: 1200, friction: 400 }
 const slow = { mass: 5, tension: 750, friction: 150 }
 const trans = (x, y) => `translate3d(${x}px,${y}px,0) translate3d(-50%,-50%,0)`
 
-export default function WaterComponent() {
+const WaterComponent = () => {
   const [trail, set] = useTrail(5, () => ({
     xy: [window.innerWidth / 2, window.innerHeight + 400],
 
@@ -38,4 +37,4 @@ export default function WaterComponent() {
     </>
   )
 }
-render(<WaterComponent />, document.getElementById("___gatsby"))
+export default WaterComponent
