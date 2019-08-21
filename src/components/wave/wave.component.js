27484f6d03fb2c22f330e47aa40ef9bb9fa7cdcf
topmonitoring <dodoboy9620@gmail.com>
2019-08-21 03:44:03 +0300

    changed wave component

--- src/components/wave/wave.component.js
+++ src/components/wave/wave.component.js
@@ -60,21 +60,3 @@ const BigWave = ({ className }) => (
 
 export const BigWaveUp = () => <BigWave className={"up-wave"} />
 export const BigWaveDown = () => <BigWave className={"down-wave"} />
-
-const BigwwWave = ({ className }) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    id="contact-wave"
-    viewBox="0 0 800 338.05"
-    preserveAspectRatio="none"
-  >
-    <path d="M 0 100 Q 165.56 287.759 400 200 Q 634.44 112.241 800 300 L 800 0 L 0 0 L 0 100 Z">
-      <animate
-        attributeName="d"
-        values="M 0 100 Q 250 50 400 200 Q 550 350 800 300 L 800 0 L 0 0 L 0 100 Z;M 0 100 Q 200 150 400 200 Q 600 250 800 300 L 800 0 L 0 0 L 0 100 Z;M 0 100 Q 150 350 400 200 Q 650 50 800 300 L 800 0 L 0 0 L 0 100 Z"
-        repeatCount="indefinite"
-        dur="30s"
-      ></animate>
-    </path>
-  </svg>
-)
