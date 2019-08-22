b30ab387cfde7d03d339f0053a8e85724cea741d
topmonitoring <dodoboy9620@gmail.com>
2019-08-22 18:46:28 +0300

    changed animated button to be reusable

--- src/components/button/animated-button.js
+++ src/components/button/animated-button.js
@@ -11,20 +11,22 @@ const CustomAnimatedButton = ({ children, ...OtherProps }) => {
   })
   return (
     <div onClick={() => toggle(!state)} {...OtherProps}>
-      <animated.div
-        {...OtherProps}
-        className="animated-button"
-        style={{
-          transform: x
-            .interpolate({
-              range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
-              output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1],
-            })
-            .interpolate(x => `scale(${x})`),
-        }}
-      >
-        {children}
-      </animated.div>
+      <button className="button-fill">
+        <animated.div
+          {...OtherProps}
+          className="animated-button"
+          style={{
+            transform: x
+              .interpolate({
+                range: [0, 0.25, 0.35, 0.45, 0.55, 0.65, 0.75, 1],
+                output: [1, 0.97, 0.9, 1.1, 0.9, 1.1, 1.03, 1],
+              })
+              .interpolate(x => `scale(${x})`),
+          }}
+        >
+          {children}
+        </animated.div>
+      </button>
     </div>
   )
 }
