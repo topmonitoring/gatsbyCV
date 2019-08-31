b456fe14d07e75dc15bea0de974e269cbe074124
topmonitoring <dodoboy9620@gmail.com>
2019-08-31 18:39:43 +0300

    updated some sio info

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ const { ANALYTICS_ID, GRAFCMS_API } = process.env
 module.exports = {
   siteMetadata: {
     title: `Trident by Dobrin Dobrev`,
-    description: `My name is Dobrin Dobrev and this is my personal CV site made by gatsby and grafcms`,
+    description: `Hi there!!! My name is Dobrin Dobrev and I am a web developer, freelancer, and open source contributor. This is my personal site where you can see my latest projects, skills and what I'm up to. If you are starting a business and need some help with your website don't hesitate to contact me. I specialize in creating static websites which are super fast and easy to use with amazing designs, SEO friendly, work great on mobile and come with  FREE HOSTING by Netlify.`,
     author: `@iamdobri`,
   },
   plugins: [
